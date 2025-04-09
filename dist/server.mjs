0a8affb17c76aa7e3d788e6f77ed28fbb31d6b61
zhbforum <lyosha.minakoff@gmail.com>
2025-04-09 11:19:30 +0200

    Lab5 done!

--- dist/server.mjs
+++ dist/server.mjs
@@ -1,4 +1,7 @@
-import data from './data.json';
+var message = "Some data";
+var data = {
+	message: message
+};
 
 console.log('Testing JSON import...');
 console.log('JSON:', data);
---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import run from "@rollup/plugin-run";
+
+const isDev = process.env.ROLLUP_WATCH === 'true';
+
+export default 
+{
+    input: 'script.js',
+    output: 
+    {
+      file: './dist/server.mjs',
+      format: 'es',
+    },
+    plugins: 
+    [
+      nodeResolve(),
+      commonjs(),
+      json(),
+      isDev && run()
+    ]
+};
